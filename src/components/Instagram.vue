c5b839a7ce9261fd453f35776580f8203f67af6d
Oleksii M <oleksiimazur111@gmail.com>
2020-02-20 17:39:41 +0200

    Initialised vue-insta test progect

---
+++ src/components/Instagram.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="instagram">
+    <h1>
+      {{ pageTitle }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Instagram',
+  data() {
+    return {
+      pageTitle: 'Vue-insta test',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .instagram {
+
+  }
+</style>
